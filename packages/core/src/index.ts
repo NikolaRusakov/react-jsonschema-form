01626ead5bf0b1773149d8c4c2e4ee2e5b6443cc
Nikola Rusakov <rusakov.nikola@gmail.com>
2025-07-10 11:18:32 +1000

    fix(core): add fields export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,9 @@
 import Form, { FormProps, FormState, IChangeEvent } from './components/Form';
 import withTheme, { ThemeProps } from './withTheme';
 import getDefaultRegistry from './getDefaultRegistry';
+import fields from './components/fields';
 
 export type { FormProps, FormState, IChangeEvent, ThemeProps };
 
-export { withTheme, getDefaultRegistry };
+export { fields, withTheme, getDefaultRegistry };
 export default Form;
